f8a63ca79d8b33530904979cc9fb11c77651bfd4
mickaelyoshua <mickaelyoshua@gmail.com>
2025-08-23 13:51:09 -0300

    finished project minigrep

--- projects/minigrep/src/main.rs
+++ projects/minigrep/src/main.rs
@@ -4,6 +4,7 @@ use std::fs;
 use std::process; // to read the file
 
 use minigrep::search;
+use minigrep::search_case_insensitive;
 
 fn main() {
     // the first value in the vector is the name of the running binary
@@ -13,15 +14,12 @@ fn main() {
                                 // args have type explicit so the return from collect() is inferred
 
     let config = Config::build(&args).unwrap_or_else(|err| {
-        println!("Problem parsing arguments: {err}");
+        eprintln!("Problem parsing arguments: {err}");
         process::exit(1);
     }); // non-panic handling error
 
-    println!("Searching for '{}'", config.query);
-    println!("In file '{}'", config.file_path);
-
     if let Err(e) = run(config) {
-        println!("Application error: {e}");
+        eprintln!("Application error: {e}");
         process::exit(1);
     } // execute if run return an error value
     // made this way because it only matter if returns an error or not
@@ -30,10 +28,16 @@ fn main() {
 fn run(config: Config) -> Result<(), Box<dyn Error>> {
     // Box<dyn Error> means that will return a type the implements the Error trait
     
-    let content = fs::read_to_string(config.file_path)?;
+    let contents = fs::read_to_string(config.file_path)?;
     // ? will return the error value from the current function
     
-    for line in search(&config.query, &content) {
+    let result = if config.ignore_case {
+        search_case_insensitive(&config.query, &contents)
+    } else {
+        search(&config.query, &contents)
+    };
+    
+    for line in result {
         println!("{line}");
     }
 
@@ -43,6 +47,7 @@ fn run(config: Config) -> Result<(), Box<dyn Error>> {
 struct Config {
     query: String,
     file_path: String,
+    ignore_case: bool,
 }
 
 impl Config {
@@ -50,6 +55,7 @@ impl Config {
         Config {
             query: args[1].clone(),
             file_path: args[2].clone(),
+            ignore_case: env::var("IGNORE_CASE").is_ok(),
         }
     }
 
