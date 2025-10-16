143cf0bac0c556bdfb01cb5cd5695f45b1b87d21
mickaelyoshua <mickaelyoshua@gmail.com>
2025-10-16 17:04:00 -0300

    more

--- study/concurrency/src/main.rs
+++ study/concurrency/src/main.rs
@@ -1,11 +1,13 @@
+// Any number of futures
 use std::time::Duration;
+use std::pin::{ Pin, pin };
 
 fn main() {
     trpl::run(async {
         let (tx, mut rx) = trpl::channel();
 
         let tx1 = tx.clone();
-        let tx_fut1 = async move {
+        let tx_fut1 = pin!( async move {
             let vals = vec![
                 String::from("hi"),
                 String::from("from"),
@@ -17,9 +19,9 @@ fn main() {
                 tx1.send(val).unwrap();
                 trpl::sleep(Duration::from_millis(500)).await;
             }
-        };
+        });
 
-        let tx_fut = async move {
+        let tx_fut = pin!( async move {
             let vals = vec![
                 String::from("more"),
                 String::from("messages"),
@@ -31,17 +33,67 @@ fn main() {
                 tx.send(val).unwrap();
                 trpl::sleep(Duration::from_millis(500)).await;
             }
-        };
+        });
 
-        let rx_fut = async {
+        let rx_fut = pin!( async {
             while let Some(value) = rx.recv().await {
                 println!("{value}");
             }
-        };
+        });
 
-        trpl::join3(tx_fut, tx_fut1, rx_fut).await;
+        let futures: Vec<Pin<&mut dyn Future<Output = ()>>> =
+            vec![tx_fut, tx_fut1, rx_fut];
+
+        trpl::join_all(futures).await;
     })
 }
+
+
+// // Concurrency with async
+// use std::time::Duration;
+//
+// fn main() {
+//     trpl::run(async {
+//         let (tx, mut rx) = trpl::channel();
+//
+//         let tx1 = tx.clone();
+//         let tx_fut1 = async move {
+//             let vals = vec![
+//                 String::from("hi"),
+//                 String::from("from"),
+//                 String::from("the"),
+//                 String::from("future"),
+//             ];
+//
+//             for val in vals {
+//                 tx1.send(val).unwrap();
+//                 trpl::sleep(Duration::from_millis(500)).await;
+//             }
+//         };
+//
+//         let tx_fut = async move {
+//             let vals = vec![
+//                 String::from("more"),
+//                 String::from("messages"),
+//                 String::from("for"),
+//                 String::from("you"),
+//             ];
+//
+//             for val in vals {
+//                 tx.send(val).unwrap();
+//                 trpl::sleep(Duration::from_millis(500)).await;
+//             }
+//         };
+//
+//         let rx_fut = async {
+//             while let Some(value) = rx.recv().await {
+//                 println!("{value}");
+//             }
+//         };
+//
+//         trpl::join3(tx_fut, tx_fut1, rx_fut).await;
+//     })
+// }
 
 // // Futures and async
 // use trpl::Html;
