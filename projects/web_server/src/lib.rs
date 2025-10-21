cf743b5aa8a15bf0878a6f3d300f8ebc7a4cbed2
mickaelyoshua <mickaelyoshua@gmail.com>
2025-10-21 08:47:01 -0300

    finished web server project

--- projects/web_server/src/lib.rs
+++ projects/web_server/src/lib.rs
@@ -1,4 +1,5 @@
 use std::thread;
+use std::sync::{mpsc, Arc, Mutex};
 
 struct Worker {
     id: usize,
@@ -6,33 +7,65 @@ struct Worker {
 }
 
 impl Worker {
-    fn new(id: usize) -> Self {
-        let thread = thread::spawn(|| {});
+    fn new(id: usize, receiver: Arc<Mutex<mpsc::Receiver<Job>>>) -> Self {
+        let thread = thread::spawn(move || {
+            loop {
+                let message = receiver.lock().unwrap().recv();
+                
+                match message {
+                    Ok(job) => {
+                        println!("Worker {id} got a job; executing...");
+                        job();
+                    },
+                    Err(_) => {
+                        println!("Worker {id} disconnected; shutting down...");
+                        break;
+                    }
+                }
+            }
+        });
         Self { id, thread }
     }
 }
 
+type Job =Box<dyn FnOnce() + Send + 'static>;
+
 pub struct ThreadPool {
     workers: Vec<Worker>,
+    sender: Option<mpsc::Sender<Job>>,
 }
 
 impl ThreadPool {
     pub fn new(size: usize) -> Self {
         assert!(size > 0);
 
+        let (sender, receiver) = mpsc::channel();
+        let receiver = Arc::new(Mutex::new(receiver));
         let mut workers = Vec::with_capacity(size);
 
         for i in 0..size {
-            workers.push(Worker::new(i));
+            workers.push(Worker::new(i, Arc::clone(&receiver)));
         }
 
-        Self { workers }
+        Self { workers, sender: Some(sender) }
     }
 
     pub fn execute<F>(&self, f: F)
     where
         F: FnOnce() + Send + 'static,
     {
+        let job = Box::new(f);
+        self.sender.as_ref().unwrap().send(job).unwrap();
+    }
+}
 
+impl Drop for ThreadPool {
+    fn drop(&mut self) {
+        drop(self.sender.take());
+        for worker in self.workers.drain(..) { // drain will remove the passed range
+            // and return an iterator
+            println!("Shutting down worker {}", worker.id);
+            worker.thread.join().unwrap();
+        }
     }
 }
